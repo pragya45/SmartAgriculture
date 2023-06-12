edda248483642e6dbaa20df9cd8023911826ceb8
Aayushbahadurpradhan <pradhanaayush9841@gmail.com>
2023-06-12 11:01:35 +0545

    starting of code

--- lib/profile/profilescreen.dart
+++ lib/profile/profilescreen.dart
@@ -1,1 +1,55 @@
-//profile
+import 'package:flutter/material.dart';
+
+class ProfileScreen extends StatefulWidget {
+  @override
+  _ProfileScreenState createState() => _ProfileScreenState();
+}
+
+class _ProfileScreenState extends State<ProfileScreen> {
+  TextEditingController _usernameController = TextEditingController();
+  bool _isEditing = false;
+
+
+  @override
+  Widget build(BuildContext context) {
+    if (user == null) {
+      return Scaffold(
+        appBar: AppBar(
+          title: Text('Profile'),
+          backgroundColor: Colors.green,
+        ),
+        body: Center(
+          child: CircularProgressIndicator(),
+        ),
+      );
+    }
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Profile'),
+        backgroundColor: Colors.green,
+      ),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(16),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              SizedBox(height: 8),
+              _isEditing
+                  ? TextFormField(
+                controller: _usernameController,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Username',
+                  hintText: 'Enter username',
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
