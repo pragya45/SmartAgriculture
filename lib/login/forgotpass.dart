329020975e3851c87709b4c28d8529f8793a7a58
deeya2002 <diyadangol2002@gmail.com>
2023-06-13 17:23:27 +0545

    forget password beginning

--- lib/login/forgotpass.dart
+++ lib/login/forgotpass.dart
@@ -1,1 +1,48 @@
-creating folder
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+
+class ForgotScreen extends StatefulWidget {
+  const ForgotScreen({Key? key}) : super(key: key);
+
+  @override
+  _ForgotScreenState createState() => _ForgotScreenState();
+}
+
+class _ForgotScreenState extends State<ForgotScreen> {
+  final email = TextEditingController();
+  final form = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: SingleChildScrollView(
+          child: Form(
+            key: form,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+
+            ElevatedButton(
+            style: ElevatedButton.styleFrom(
+            backgroundColor: Colors.greenAccent,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(30),
+              ),
+            ),
+            onPressed: () {
+              passwordReset();
+            },
+            SizedBox(
+              height: 10,
+            ),
+            ],
+          ),
+        ),
+      ),
+    ),
+    );
+  }
+}
