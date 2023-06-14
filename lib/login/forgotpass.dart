74aa6e362bf91922da339ce1c35913e02c31ec46
deeya2002 <diyadangol2002@gmail.com>
2023-06-14 16:18:58 +0545

    forget password code

--- lib/login/forgotpass.dart
+++ lib/login/forgotpass.dart
@@ -2,6 +2,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+import 'Login.dart';
 
 class ForgotScreen extends StatefulWidget {
   const ForgotScreen({Key? key}) : super(key: key);
@@ -12,6 +13,7 @@ class ForgotScreen extends StatefulWidget {
 
 class _ForgotScreenState extends State<ForgotScreen> {
   final email = TextEditingController();
+
   final form = GlobalKey<FormState>();
 
   @override
@@ -24,25 +26,50 @@ class _ForgotScreenState extends State<ForgotScreen> {
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                  child: Container(
+                    child: Text(
+                      'Enter your email and we will send you a password reset link.',
+                      style: TextStyle(
+                        fontSize: 20,
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 30,
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(25.0),
+                  child: Container(
+                    height: 60,
+                    decoration: BoxDecoration(
+                        color: const Color(0xffB4B4B4).withOpacity(0.6),
+                        borderRadius: BorderRadius.circular(30)),
+                    child: Padding(
+                      padding: EdgeInsets.only(left: 10),
+                      child: Center(
+                        child: TextFormField(
+                          controller: email,
+                          decoration: InputDecoration(
+                            border: InputBorder.none,
+                            hintText: 'Email',
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
 
-            ElevatedButton(
-            style: ElevatedButton.styleFrom(
-            backgroundColor: Colors.greenAccent,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(30),
-              ),
+                SizedBox(
+                  height: 10,
+                ),
+              ],
             ),
-            onPressed: () {
-              passwordReset();
-            },
-            SizedBox(
-              height: 10,
-            ),
-            ],
           ),
         ),
       ),
-    ),
     );
   }
 }
