03a54e844ed92294536763b59fa24fe5111649ab
Aayushbahadurpradhan <pradhanaayush9841@gmail.com>
2023-06-15 19:18:58 +0545

    finished the profile

--- lib/profile/profilescreen.dart
+++ lib/profile/profilescreen.dart
@@ -10,15 +10,89 @@ class ProfileScreen extends StatefulWidget {
 }
 
 class _ProfileScreenState extends State<ProfileScreen> {
+  User? user;
+  late DocumentSnapshot userData;
   TextEditingController _usernameController = TextEditingController();
+  TextEditingController _passwordController = TextEditingController();
+  TextEditingController _confirmPasswordController = TextEditingController();
   bool _isEditing = false;
 
   @override
   void initState() {
     super.initState();
+    fetchUserData();
   }
 
-  
+  void fetchUserData() async {
+    final User? currentUser = FirebaseAuth.instance.currentUser;
+    if (currentUser != null) {
+      final userData = await FirebaseFirestore.instance
+          .collection('users')
+          .doc(currentUser.uid)
+          .get();
+      setState(() {
+        user = currentUser;
+        this.userData = userData;
+        _usernameController.text = userData.get('username') ?? '';
+      });
+    }
+  }
+
+  void _logout(BuildContext context) async {
+    final confirmed = await showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Logout'),
+          content: Text('Are you sure you want to logout?'),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop(false);
+              },
+              child: Text('No'),
+            ),
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop(true);
+              },
+              child: Text('Yes'),
+            ),
+          ],
+        );
+      },
+    );
+
+    if (confirmed == true) {
+      FirebaseAuth.instance.signOut();
+      Navigator.of(context).pushReplacementNamed('/login');
+    }
+  }
+
+  void _saveProfile() async {
+    final User? currentUser = FirebaseAuth.instance.currentUser;
+    if (currentUser != null) {
+      if (_usernameController.text.isNotEmpty) {
+        await FirebaseFirestore.instance
+            .collection('users')
+            .doc(currentUser.uid)
+            .update({
+          'username': _usernameController.text,
+        });
+      }
+
+      setState(() {
+        _isEditing = false;
+      });
+
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text('Profile updated successfully!'),
+        ),
+      );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     if (user == null) {
