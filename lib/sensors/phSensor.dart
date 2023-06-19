fdeac98786254876026cadcaa2e831d2c66c3833
Aayushbahadurpradhan <pradhanaayush9841@gmail.com>
2023-06-19 10:57:38 +0545

    added some code

--- lib/sensors/phSensor.dart
+++ lib/sensors/phSensor.dart
@@ -1,1 +1,23 @@
-//craeted file
+import 'package:flutter/material.dart';
+
+class PHValueSensor extends StatefulWidget {
+  @override
+  _PHValueSensorState createState() => _PHValueSensorState();
+}
+
+class _PHValueSensorState extends State<PHValueSensor> {
+
+  List<Map<String, dynamic>> sensorData = [];
+
+
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('pH Value Sensor'),
+      ),
+      body: Center(
+      ),
+    );
+  }
+}
